f8f3dbb37993f8002ab868ab35d0893375287aa3
plum-bee <rafakanex1@gmail.com>
2024-01-12 17:21:57 +0100

    Styled splash screen

--- lib/src/pages/splash_screen.dart
+++ lib/src/pages/splash_screen.dart
@@ -5,7 +5,7 @@ class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() => _SplashScreenState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
@@ -20,14 +20,37 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/images/school_logo.png', width: 150.0),
-            const SizedBox(height: 20.0),
-            const CircularProgressIndicator(),
-          ],
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.blue,
+              Colors.red,
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              AnimatedOpacity(
+                opacity: 1.0,
+                duration: const Duration(seconds: 2),
+                child:
+                    Image.asset('assets/images/school_logo.png', width: 200.0),
+              ),
+              const SizedBox(height: 30.0),
+              const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+              const SizedBox(height: 20.0),
+              const Text('Loading...', style: TextStyle(color: Colors.white)),
+              const Text('Version 1.0.0',
+                  style: TextStyle(color: Colors.white)),
+            ],
+          ),
         ),
       ),
     );
